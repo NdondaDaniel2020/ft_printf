306e4b3968ab89cf8851421d29c5a09bb5bc2e69
NdondaDaniel2020 <kali@kali>
2024-10-25 15:43:49 +0100

    retificacao no caso base

--- bonus/ft_format_bonus.c
+++ bonus/ft_format_bonus.c
@@ -12,6 +12,34 @@
 
 #include "ft_printf_bonus.h"
 
+static void	add_format_one_space(t_data *data, int *i)
+{
+	int			len;
+	long long	value;
+	va_list		cpy_arg;
+
+	va_copy(cpy_arg, data->args);
+	if (data->str[*i] == 'd' || data->str[*i] == 'i' || data->str[*i] == 'p')
+	{
+		len = 0;
+		value = 0;
+
+		if (data->str[*i] == 'p')
+			value = va_arg(cpy_arg, long long);		
+		else
+			value = va_arg(cpy_arg, int);
+		if (value > 0)
+		{
+			data->len_caraters++;
+			ft_putchar_fd(' ', 1);
+		}
+	}
+	if (data->str[*i] == 's')
+		len = ft_strlen(va_arg(cpy_arg, char *));
+	va_end(cpy_arg);
+	data->len_caraters = ft_print_pars(data->str[*i], data->args, data->len_caraters, len);
+}
+
 static void	add_format_plus(t_data *data, int *i)
 {
 	va_list 	cpy_arg;
@@ -26,13 +54,17 @@ static void	add_format_plus(t_data *data, int *i)
 	data->len_caraters = ft_print_pars(data->str[*i], data->args, data->len_caraters, 0);
 }
 
+
+
 static void add_format_comb_number(t_data *data, int *i)
 {
 	int			len;
 	int			value;
 	char		chr;
+	bool		es_case;
 	va_list 	cpy_arg;
 
+	es_case = false;
 	va_copy(cpy_arg, data->args);
 	value = va_arg(cpy_arg, int);
 	len = ft_nblen(value, 10);
@@ -40,6 +72,7 @@ static void add_format_comb_number(t_data *data, int *i)
 		chr = '0';
 	else
 		chr = ' ';
+
 	if (data->plas && value >= 0 && data->str[*i] != 'u')
 	{
 		len++;
@@ -47,18 +80,35 @@ static void add_format_comb_number(t_data *data, int *i)
 		if (data->zero)
 			ft_putchar_fd('+', 1);
 	}
+
+	if (data->zero && data->space && value >= 0)
+	{
+		ft_putchar_fd(' ', 1);
+		len++;
+	}
 	data->number -= len;
+	if (data->zero && value < 0 && (data->str[*i] == 'i' || data->str[*i] == 'd'))
+	{
+		data->len_caraters++;
+		va_arg(data->args, int);
+		ft_putchar_fd('-', 1);
+		value *= -1;
+	}
 	while (data->number--)
 	{
 		data->len_caraters++;
 		ft_putchar_fd(chr, 1);
 	}
+
 	if (data->plas && value >= 0  && data->str[*i] != 'u')
 	{
 		if (!data->zero && data->str[*i] != 'u')
 			ft_putchar_fd('+', 1);
 	}
-	data->len_caraters = ft_print_pars(data->str[*i], data->args, data->len_caraters, 0);
+	if (es_case)
+		data->len_caraters = ft_print_pars(data->str[*i], data->args, data->len_caraters, 0);
+	else
+		data->len_caraters = ft_putnbr_base(value, 10, 0, data->len_caraters);
 	va_end(cpy_arg);
 }
 
@@ -72,6 +122,12 @@ static void add_format_comb_number_reverse(t_data *data, int *i)
 	
 	value = va_arg(cpy_arg, int);
 	len = ft_nblen(value, 10);
+	if (data->zero && data->space)
+	{
+		data->len_caraters++;
+		ft_putchar_fd(' ', 1);
+		len++;
+	}
 	if (data->plas && value >= 0)
 	{
 		len++;
@@ -87,6 +143,9 @@ static void add_format_comb_number_reverse(t_data *data, int *i)
 	}
 	va_end(cpy_arg);
 }
+
+
+
 
 static void add_format_comb_str(t_data *data, int *i)
 {
@@ -134,28 +193,31 @@ static void add_format_comb_str_reverse(t_data *data, int *i)
 	va_end(cpy_arg);
 }
 
+
 void	print_format(t_data *data, int *i)
 {
 	// '-', '0', '.', '#', ' ', '+'
-	if (data->space && !data->plas &&
-	   !data->negative && !data->zero &&
-	   !data->point && !data->hastag && !data->number)
-	{
-		data->len_caraters++;
-		ft_putchar_fd(' ', 1);
-	}
-	else if (data->plas && !data->space &&
-			!data->negative && !data->zero &&
-			!data->point && !data->hastag && !data->number && 
-			(data->str[*i] == 'i' || data->str[*i] == 'd' || data->str[*i] == 'p'))
+	if (data->space && !data->plas && !data->negative && !data->zero && !data->point && !data->hastag && !data->number)
+		add_format_one_space(data, i);
+	else if (data->plas && !data->space && !data->negative && !data->zero && !data->point && !data->hastag && !data->number && (data->str[*i] == 'i' || data->str[*i] == 'd' || data->str[*i] == 'p'))
 		add_format_plus(data, i);
-	else if (data->number && !data->negative &&
-			!data->point && !data->hastag && data->str[*i] != 's' && data->str[*i] != 'c')
+	
+	
+	else if (data->number && !data->negative && !data->point && !data->hastag && data->str[*i] != 's' && data->str[*i] != 'c')
 		add_format_comb_number(data, i);
 	else if (data->number && data->negative && !data->point && !data->hastag && data->str[*i] != 's' && data->str[*i] != 'c')
 		add_format_comb_number_reverse(data, i);
+	
+
+	// else if (data->number && data->poin   t && !data->negative && !data->hastag && data->str[*i] != 's' && data->str[*i] != 'c')
+		// add_format_comb_number_point(data, i);
+
+
 	else if ((data->number || data->number_str) && !data->negative && !data->hastag && (data->str[*i] == 's' || data->str[*i] == 'c'))
 		add_format_comb_str(data, i);
 	else if ((data->number || data->number_str) && data->negative && !data->hastag && (data->str[*i] == 's' || data->str[*i] == 'c'))
 		add_format_comb_str_reverse(data, i);
+	// else if (data->hastag && (data->str[*i] == 'X' || data->str[*i] == 'x'))
+	// {
+	// }
 }
--- main.c
+++ main.c
@@ -21,71 +21,52 @@ int main(void)
     char str[] = "OpenAI";
     void *ptr = &num;
 
-    // %-10c: Caractere alinhado à esquerda com largura mínima de 10.
+    
+	// %-10c: Caractere alinhado à esquerda com largura mínima de 10.
     //    printf("'% 1c'\n", ch);
 	// ft_printf("'% 1c'\n", ch);
 
-	int i1 =    printf("'%5.1s'\n", str);
-	int i2 = ft_printf("'%5.1s'\n", str);
+	// int i1 =    printf("'%-10.10d'\n", num);
+	// int i2 = ft_printf("'%-10.d'\n", num);
+
+
+	//    printf("'%10.5d'\n", 123);   // Saída: "     00123"
+	// ft_printf("'%10.5d'\n", 123);   // Saída: "     00123"
+ 
+	//    printf("'%10.5d'\n", -123);  // Saída: "    -00123"
+	// ft_printf("'%10.5d'\n", -123);  // Saída: "    -00123"
+	
+	//    printf("'%+10.5d'\n", 123);  // Saída: "    +00123"
+	// ft_printf("'%+10.5d'\n", 123);  // Saída: "    +00123"
+
+	//    printf("'%+10.5d'\n", -123); // Saída: "    -00123"
+	// ft_printf("'%+10.5d'\n", -123); // Saída: "    -00123"
+	
+	//    printf("'%-10.5d'\n", 123);  // Saída: "+00123    "
+	// ft_printf("'%-10.5d'\n", 123);  // Saída: "+00123    "
+
+	//    printf("'%-10.5d'\n", -123); // Saída: "-00123    "
+	// ft_printf("'%-10.5d'\n", -123); // Saída: "-00123    "
+	
+	//    printf("'%010.5d'\n", 123);   // Saída: "     00123"
+	// ft_printf("'%010.5d'\n", 123);   // Saída: "     00123"
+	
+	//    printf("'%010.5d'\n", -123);  // Saída: "    -00123"
+	// ft_printf("'%010.5d'\n", -123);  // Saída: "    -00123"
+	
+	//    printf("'%+010.5d'\n", 123);   // Saída: "    +00123"
+	// ft_printf("'%+010.5d'\n", 123);   // Saída: "    +00123"
+	
+	//    printf("'%+010.5d'\n", -123);  // Saída: "    -00123"
+	// ft_printf("'%+010.5d'\n", -123);  // Saída: "    -00123"
+	
+	//    printf("'% 010.5d'\n", 123);   // Saída: "     00123"
+	// ft_printf("'% 010.5d'\n", 123);   // Saída: "     00123"
+	
+   	// printf("'% 010.5d'\n", -123);  // Saída: "    -00123"
+	// ft_printf("'% 010.5d'\n", -123);  // Saída: "    -00123"
 
 	// printf("(%i %i)\n", i1, i2);
 
-    // // %+10d: Número com sinal positivo ou negativo, largura mínima de 10.
-    ft_printf("'%+10d'\n", num);
-	   printf("'%+10d'\n", num);
-
-    // // % 10d: Número com espaço antes de números positivos, largura mínima de 10.
-    // ft_printf("'% 10d'\n", num);
-	//    printf("'% 10d'\n", num);
-
-    // // %010d: Número com zeros à esquerda, largura mínima de 10.
-    // ft_printf("'%010d'\n", num);
-	//    printf("'%010d'\n", num);
-
-    // // %#x: Número hexadecimal com prefixo "0x".
-    // ft_printf("'%#x'\n", num);
-	//    printf("'%#x'\n", num);
-
-    // // %#X: Número hexadecimal com prefixo "0X" (maiúsculas).
-    // ft_printf("'%#X'\n", num);
-	//    printf("'%#X'\n", num);
-
-    // // %-10s: String alinhada à esquerda com largura mínima de 10.
-    // ft_printf("'%-10s'\n", str);
-	//    printf("'%-10s'\n", str);
-
-    // // %.3s: String com precisão de 3 caracteres.
-    // ft_printf("'%.3s'\n", str);
-	//    printf("'%.3s'\n", str);
-
-    // %10u: Número sem sinal, com largura mínima de 10.
-    // ft_printf("'%10u'\n", unum);
-	//    printf("'%10u'\n", unum);
-
-    // // %+10i: Número inteiro com sinal positivo ou negativo, largura mínima de 10.
-    // ft_printf("'%+10i'\n", num);
-	//    printf("'%+10i'\n", num);
-
-    // // %%: Imprime o sinal de porcentagem.
-    // ft_printf("'%%'\n");
-	//    printf("'%%'\n");
-
     return 0;
 }
-
-
-/*
-int add_prefix(const char *str, int i, int len)
-{
-	if (str[i + 1] == 'x' || str[i + 1] == 'X')
-	{
-		len += 2;
-		ft_putchar_fd('0', 1);
-		if (str[i + 1] == 'x')
-			ft_putchar_fd('x', 1);
-		if (str[i + 1] == 'X')
-			ft_putchar_fd('X', 1);
-	}
-	return (len);
-}
-*/
